50b748e1f8393680af7e8d07b0faab0eb3e6aaaa
Thibault Virsolvy <Thibault Virsolvy>
2017-05-15 12:54:34 +0200

    removed dead code

--- app/src/main/java/com/projet/mti/velibparis/MainActivity.java
+++ app/src/main/java/com/projet/mti/velibparis/MainActivity.java
@@ -1,9 +1,5 @@
 package com.projet.mti.velibparis;
 
-import android.app.SearchManager;
-import android.content.Context;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -11,7 +7,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
 
@@ -28,12 +23,6 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_menu, menu);
 
-        /*SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));*/
         SearchView searchView = (SearchView)menu.findItem(R.id.search).getActionView();
         searchView.setOnQueryTextListener(this);
 
@@ -52,17 +41,4 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         Log.e("QUERY", "Query : " + newText);
         return false;
     }
-
-
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-            case R.id.action_favorite:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }*/
 }
