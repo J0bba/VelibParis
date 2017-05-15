a0bfbf1a7ad63e3679970fde85a002ced23f0488
Thibault Virsolvy <Thibault Virsolvy>
2017-05-15 12:38:32 +0200

    added the search toolbar

--- app/src/main/java/com/projet/mti/velibparis/MainActivity.java
+++ app/src/main/java/com/projet/mti/velibparis/MainActivity.java
@@ -1,11 +1,19 @@
 package com.projet.mti.velibparis;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,44 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+
+        /*SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));*/
+        SearchView searchView = (SearchView)menu.findItem(R.id.search).getActionView();
+        searchView.setOnQueryTextListener(this);
+
+
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Log.e("QUERY", "Query : " + query);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        Log.e("QUERY", "Query : " + newText);
+        return false;
+    }
+
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_favorite:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }*/
 }
