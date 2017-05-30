aaed717d88e39fcca15051a4bf6ddf17adeff84b
Thibault Virsolvy <Thibault Virsolvy>
2017-05-30 19:46:46 +0200

    removed dead code

--- app/src/main/java/com/projet/mti/velibparis/Detail/DetailsActivity.java
+++ app/src/main/java/com/projet/mti/velibparis/Detail/DetailsActivity.java
@@ -9,29 +9,13 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.projet.mti.velibparis.API.RecordsAPI;
-import com.projet.mti.velibparis.API.WebServiceReturn;
 import com.projet.mti.velibparis.GroupDetails.GroupDetailsActivity;
 import com.projet.mti.velibparis.R;
-import com.projet.mti.velibparis.StationItem;
 import com.projet.mti.velibparis.StationItemsList;
-import com.projet.mti.velibparis.WebService;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class DetailsActivity extends AppCompatActivity {
@@ -49,52 +33,6 @@ public class DetailsActivity extends AppCompatActivity {
                 return i;
         }
         return 0;
-    }
-
-    public void collectData()
-    {
-        //stations = new ArrayList<>();
-        final Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(WebService.ENDPOINT)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        WebService webService = retrofit.create(WebService.class);
-        Call<WebServiceReturn> webServiceReturnCall = webService.callService("stations-velib-disponibilites-en-temps-reel", 100);
-        webServiceReturnCall.enqueue(new Callback<WebServiceReturn>() {
-            @Override
-            public void onResponse(Call<WebServiceReturn> call, Response<WebServiceReturn> response) {
-                if(response.isSuccessful()) {
-                    WebServiceReturn returnService = response.body();
-                    ArrayList<RecordsAPI> recordsAPIs = returnService.getRecords();
-                    for(int i = 0; i < recordsAPIs.size(); i++)
-                    {
-                        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ssZ");
-                        Date update = null;
-                        try {
-                            update = df.parse(recordsAPIs.get(i).getFields().getLast_update());
-                        }
-                        catch (java.text.ParseException e)
-                        {}
-                        stations.add(new StationItem(recordsAPIs.get(i).getFields().getStatus(),
-                                                     recordsAPIs.get(i).getFields().getName(),
-                                                     recordsAPIs.get(i).getFields().getBike_stands(),
-                                                     recordsAPIs.get(i).getFields().getAvailable_bike_stands(),
-                                                     recordsAPIs.get(i).getFields().getAddress(),
-                                                     update,
-                                                     recordsAPIs.get(i).getFields().getPosition().get(0),
-                                                     recordsAPIs.get(i).getFields().getPosition().get(1)));
-                    }
-                    adapter.setData(stations);
-                    viewPager.setCurrentItem(getIndexByName(getIntent().getStringExtra("Station")), false);
-                } else {
-                    //Error
-                }
-            }
-            @Override
-            public void onFailure(Call<WebServiceReturn> call, Throwable t) {
-                Log.e("APITEST", t.getMessage());
-            }
-        });
     }
 
 
